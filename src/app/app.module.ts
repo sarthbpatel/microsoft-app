dff28b3bf3fc058e5e2d19f1ffe8f5f13ca602a8
Sarth Patel <sarthbpatel@gmail.com>
2022-01-26 21:59:55 -0500

    Added logos, added icons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule
