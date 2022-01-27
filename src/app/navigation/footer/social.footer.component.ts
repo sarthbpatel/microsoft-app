dd5a59abdfe0e8743ea1d6c9707bbaff68955d18
Sarth Patel <sarthbpatel@gmail.com>
2022-01-27 03:49:32 -0500

    Added second footernavbar

---
+++ src/app/navigation/footer/social.footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'ms-social-footer',
+    templateUrl: 'social.footer.component.html',
+    styleUrls: ['social.footer.component.html']
+})
+export class SocialFooterComponent {
+
+}
