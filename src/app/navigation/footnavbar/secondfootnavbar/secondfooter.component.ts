ae8ee17ead1fcec0258771ed525852e4c33db5d2
Sarth patel <sarthbpatel@gmail.com>
2022-02-01 22:35:11 -0500

    Updated file location for footer navbar

---
+++ src/app/navigation/footnavbar/secondfootnavbar/secondfooter.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:"ms-secondfooter",
+    templateUrl: "secondfooter.component.html",
+    styleUrls: ["secondfooter.component.css"]
+})
+export class SecondFooterComponent {
+
+}
