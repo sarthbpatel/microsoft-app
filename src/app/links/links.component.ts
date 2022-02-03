a5109e5b661c71a5f5418f49194c9a6c655bc4e5
Sarth patel <sarthbpatel@gmail.com>
2022-02-03 04:44:32 -0500

    Created links component

---
+++ src/app/links/links.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "ms-linkscomponent",
+    templateUrl: "links.component.html",
+    styleUrls: ["links.component.css"]
+})
+export class LinksComponent {
+    
+}
