3f6a30f9d7869ec1e98e909e070a7e0a6016a73b
Sarth patel <sarthbpatel@gmail.com>
2022-02-03 04:42:58 -0500

    Created Jumbotron component

---
+++ src/app/jumbotron/jumbotron.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector:"ms-jumbotron",
+    templateUrl: "jumbotron.component.html",
+    styleUrls:["jumbotron.component.css"]
+})
+export class JumbotronComponent {
+
+}
