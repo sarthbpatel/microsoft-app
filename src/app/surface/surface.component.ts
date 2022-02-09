174f2428de52415cb01dc45dfe4089f8f4006f8c
Sarth Patel <sarthbpatel@gmail.com>
2022-02-09 01:32:51 -0500

    Created microsoft-app

---
+++ src/app/surface/surface.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "ms-surface",
+    templateUrl: "surface.component.html",
+    styleUrls: ["surface.component.css"]
+})
+export class SurfaceComponent {
+
+}
