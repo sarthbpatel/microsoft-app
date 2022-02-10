fa21ae323ad4edc592993df8b4e88b7581c09228
Sarth Patel <sarthbpatel@gmail.com>
2022-02-10 00:07:07 -0500

    Created files for data binding and mock list

---
+++ src/app/For Business/mock_product_item.ts
@@ -0,0 +1,30 @@
+export const mock_product_item_forbusiness = [
+    {
+        img: "./assets/forbusiness/surface-pro-8-business.jpeg",
+        sos: "Surface Pro 8 for Business",
+        description: "Get unprecedented levels of performance and versatility on a 13-inch screen. Available with Windows 11.",
+        link1:"Shop Now",
+        link2:"Shop Enterprise"
+    },
+    {
+        img:"./assets/forbusiness/microsoft-teams.jpeg",
+        sos: "Get Microsoft Teams for free",
+        description: "Get online meetings, chat, and shared cloud storage - all in one place with the free version of Microsoft Teams", 
+        link1:"Sign up for free",
+        link2:""
+    },
+    {
+        img:"./assets/forbusiness/windows-11.jpeg",
+        sos:"Windows 11 for business is here",
+        description:"Designed for hybrid work. Powerful for employees. Consistent for IT. Secure for all.",
+        link1:"Learn more",
+        link2:""
+    },
+    {
+        img:"./assets/forbusiness/365-business.jpeg",
+        sos:"Microsoft 365 for business", 
+        description:"Stay a step ahead with powerful apps for productivity, connection, and security. Make an appointment with an expert to determine the best solution for your business.", 
+        link1:"Shop now",
+        link2:"Book consultation"
+    }
+];
