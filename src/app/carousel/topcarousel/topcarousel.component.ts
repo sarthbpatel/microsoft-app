c753f9c8ae3b53892bfe6d34be93ff9c67bcfd5b
Sarth Patel <sarthbpatel@gmail.com>
2022-02-13 14:54:24 -0500

    Created top carousel

---
+++ src/app/carousel/topcarousel/topcarousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component ({
+    selector: "ms-topcarousel",
+    templateUrl: "topcarousel.component.html",
+    styleUrls:["topcarousel.component.css"]
+})
+
+export class TopCarouselComponent {
+
+}
