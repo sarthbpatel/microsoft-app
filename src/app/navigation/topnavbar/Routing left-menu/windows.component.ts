5a510b39b4748b3d8b4372eef43e9fd2aa3fc0e6
Sarth Patel <sarthbpatel@gmail.com>
2022-02-17 02:23:01 -0500

    Created windows component

---
+++ src/app/navigation/topnavbar/Routing left-menu/windows.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ms-windows',
+    templateUrl:'windows.component.html',
+    styleUrls: ['windows.component.css']
+})
+export class WindowsComponent {
+}
