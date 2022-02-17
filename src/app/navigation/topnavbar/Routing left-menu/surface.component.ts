e871f0871e7b16b35f78954ab238c82c733b69d4
Sarth Patel <sarthbpatel@gmail.com>
2022-02-17 02:23:19 -0500

    Created surface component

---
+++ src/app/navigation/topnavbar/Routing left-menu/surface.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ms-surfacenav',
+    templateUrl:'surface.component.html',
+    styleUrls: ['surface.component.css']
+})
+export class SurfaceNavComponent {
+
+}
