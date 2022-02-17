f60c2d3a511ae263cd984e22b0c5430b93f2f3ee
Sarth Patel <sarthbpatel@gmail.com>
2022-02-17 02:23:55 -0500

    Created microsoft365 component

---
+++ src/app/navigation/topnavbar/Routing left-menu/microsoft365.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ms-microsoft365',
+    templateUrl:'microsoft365.component.html',
+    styleUrls: ['microsoft365.component.css']
+})
+export class Microsoft365Component {
+}
