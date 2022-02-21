8af7b65f5762a81f100260dd88802528e71840f8
Sarth Patel <sarthbpatel@gmail.com>
2022-02-20 22:09:38 -0500

    Created bottom Carousel

---
+++ src/app/carousel/bottomcarousel/bottomcarousel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ms-bottomcarousel',
+    templateUrl: 'bottomcarousel.component.html',
+    styleUrls: ['bottomcarousel.component.css']
+})
+export class BottomCarouselComponent {
+
+}
