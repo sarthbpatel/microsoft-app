2207ea215870b9e3b7a24c5f3a0f30a522fba8d6
Sarth Patel <sarthbpatel@gmail.com>
2022-02-20 22:09:55 -0500

    Created COVID-19 jumbotron

---
+++ src/app/COVID-19 jumbotron/jumbotron.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component ({
+    selector: 'ms-covidjumbotron',
+    templateUrl: 'jumbotron.component.html',
+    styleUrls: ['jumbotron.component.css']
+})
+export class COVIDJumbotronComponent {
+
+}
