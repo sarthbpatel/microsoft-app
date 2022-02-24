49a20d91227c0e723c063d5c2a5d10038bcea6d9
Sarth Patel <sarthbpatel@gmail.com>
2022-02-24 03:46:12 -0500

    Created social link on home page

---
+++ src/app/sociallinks.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: "ms-sociallinks",
+    templateUrl: "sociallinks.component.html",
+    styleUrls: ["sociallinks.component.css"]
+})
+export class SocialLinksComponent {
+
+}
