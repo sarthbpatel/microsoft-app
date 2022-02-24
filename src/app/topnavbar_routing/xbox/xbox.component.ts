d40a564eccab3086ef179fa45a2f3fa31b0af643
Sarth Patel <sarthbpatel@gmail.com>
2022-02-24 03:47:29 -0500

    Created three new components in xbox routing

--- src/app/topnavbar_routing/xbox/xbox.component.ts
+++ src/app/topnavbar_routing/xbox/xbox.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { ProductItemModelXboxLink } from "./xbox_product_item_model";
+import { mock_product_item_XboxLink } from "./xbox_mock_product_item";
+@Component ({
+    selector: "ms-xbox",
+    templateUrl: "xbox.component.html",
+    styleUrls: ['xbox.component.css']
 
-@Component({
-  selector: 'ms-xbox',
-  templateUrl: './xbox.component.html',
-  styleUrls: ['./xbox.component.css']
 })
-export class XboxComponent implements OnInit {
+export class XboxComponent {
+    @Input() img: String;
+    @Input() link: String;
 
-  constructor() { }
+    xboximages = mock_product_item_XboxLink.map(image => new ProductItemModelXboxLink(image.img, image.link))
 
-  ngOnInit(): void {
-  }
+    constructor() {
+        this.img = "";
+        this.link = "";
+    }
 
 }
---
+++ src/app/topnavbar_routing/xbox/xbox_mock_product_item.ts
@@ -0,0 +1,27 @@
+export const mock_product_item_XboxLink = [
+    {
+        img: "/assets/xbox/links/gamepass.png",
+        link: "Game Pass"
+    },
+    {
+        img: "/assets/xbox/links/games.png",
+        link: "Games"
+    },
+    {
+        img:"/assets/xbox/links/consoles.png" ,
+        link: "Consoles"
+    },
+    {
+        img: "/assets/xbox/links/accessories.png",
+        link: "Accessories"
+    },
+    {
+        img: "/assets/xbox/links/play.png",
+        link: "Play"
+    },
+    {
+        img: "/assets/xbox/links/signin.png",
+        link: "Sign In"
+    }
+
+]
