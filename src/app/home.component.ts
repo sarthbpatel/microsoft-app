ec46019ca5bd0e3f349394515bfe314e0f99bd52
Sarth Patel <sarthbpatel@gmail.com>
2022-03-03 15:14:12 -0500

    Updated service file in home component

--- src/app/home.component.ts
+++ src/app/home.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { mock_product_item_forbusiness } from "./forbusiness/forbusiness_mock_product_item";
 import { ProductItemModelForBusiness } from "./forbusiness/forbusiness_product_item_model";
+import { ProductService } from "./home.services";
 import { mock_product_item_link } from "./links/links_mock_product_item";
 import { ProductItemModelLink } from "./links/links_product_item_model";
 import { mock_product_item_surface } from "./surface-cards/surfacecard_mock_product_item";
@@ -13,12 +14,13 @@ import { ProductItemModelSurface } from "./surface-cards/surfacecard_product_ite
     styleUrls: ['home.component.css']
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   products: ProductItemModelSurface[] = [];
   links: ProductItemModelLink[] = [];
   forbusiness: ProductItemModelForBusiness[] = [];
+  products_forbusiness: ProductItemModelForBusiness[] = [];
 
-    constructor() {
+    constructor(private service: ProductService) {
       for (var product of mock_product_item_surface) {
       console.log(product);
       this.products.push(product);
@@ -34,6 +36,16 @@ export class HomeComponent {
         this.forbusiness.push(forbusiness);
 
       }
-    } 
+    }
+  ngOnInit(): void {
+    console.log("Fetch data");
+    this.service.getProducts().subscribe(data => {
+      console.log(data);
+      for(var product of data) {
+        this.products_forbusiness.push(product);
+      }
+    })
+  }
+    
   
 }
